b06fa72cd8e307124db019219e23f40b830d368a
JorgeCoto98 <120097112+JorgeCoto98@users.noreply.github.com>
2023-08-02 20:40:31 +0000

    Entrega

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter" && inputValue.trim() !== "") {
+      setTodos([...todos, inputValue]);
+      setInputValue(""); // Limpiar el input después de agregar la tarea
+    }
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  const tasksRemaining = todos.length;
+
+  return (
+    <div className="container">
+      <h1>Rutina {inputValue}</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyPress={handleKeyPress}
+            placeholder="Agregar tareas"
+          />
+        </li>
+        {todos.map((task, index) => (
+          <li key={index}>
+            {task}{" "}
+            <FontAwesomeIcon
+              icon={faTimes}
+              style={{ color: "#c800ff", cursor: "pointer" }}
+              onClick={() => handleDeleteTask(index)}
+            />
+          </li>
+        ))}
+      </ul>
+	  <div>Quedan {tasksRemaining} más por hacer</div>
+    </div>
+  );
 };
 
 export default Home;
